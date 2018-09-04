8f310c76e5046e538f0fc73c4c05a63399acf8ad
Namnd-gem <namnd@gemvietnam.com>
2018-09-04 17:54:05 +0700

    initial commit

---
+++ NabtoPoC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nabto.Client;
+namespace NabtoPoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profile = Guid.NewGuid().ToString();
+            NabtoClient nabto = new NabtoClient();
+            nabto.CreateSelfSignedProfile(profile, "bar");
+            Session session = nabto.CreateSession(profile, "bar");
+            //TODO
+            Console.WriteLine("Starting Nabto...");
+            nabto.Dispose();
+
+        }
+    }
+}
